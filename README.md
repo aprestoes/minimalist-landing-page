562fae6150b49be060ee5060090b8bec1e7f5c10
Daniel Bala <daniel.bala@carleton.ca>
2020-08-11 02:12:49 -0400

    "Initial initial" commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# Dan(g)
+
+A simplistic, minimalistic, barebones (and all the other good ajectives) landing page with links to your work.
+
+![](example.gif)
+(Default options)
+
+![](example-with-options.gif)
+(With dynamic loading, rotateParagraph, and connectParagraphToHeaders enabled.)
+
+[Live site](https://aprestoes.github.io/minimalist-landing-page)
+
+Based off of my larger and as-of-yet incomplete [personal website](https://github.com/aprestoes/aprestoes.github.io).
+Features a responsive webpage, (optionally dynamically loaded) [sliding words](https://github.com/ganlanyuan/tiny-slider), social media icons, and a dark mode.
+
+## Config
+##### index.html
+Elements and sliders are loaded statically by default with the structure provided in index.html.
+To use the dark mode, uncomment line 27: ```<link rel="stylesheet" href="css/darktheme.css">```
+Before enabling ```loadDynamically```, remove all elements within "#rotating-words" and "#rotating-paragraphs".
+
+##### js/main.js
+```javascript
+/* IMPORTANT: Each array must have at least 1 element
+  and you must remove all elements within "#rotating-words"
+  and "#rotating-paragraphs". REQUIRED to use other
+  options.
+  You can also bypass the dynamic elements entirely
+  and uncomment the elements in index.html
+*/
+var loadDynamically = false;
+
+/* The beginning of the header sentence has a black font.
+  The headersBlack element of index i, will be added to
+  the corresponding headersBlue element of index i.
+  E.g. headersBlack[i] + headersBlue[i]
+  headersBlack[0] + headersBlue[0]
+  "Hi, I'm " + "Dan" = "Hi, I'm Dan"
+  
+  If one array is larger than the other, the last element
+  of the smaller array will be used for the rest of the
+  rotations. You can also use empty strings ""
+  E.g. headersBlack[1] doesn't exist
+  "Hi, I'm a student." 
+*/
+var headersBlack = [ //headersBlack + headersBlue, e.g. "Hi, I'm Dan"
+  "Hi, I'm "
+];
+
+var headersBlue = [ //Descriptive and re-affirming words here
+  "Dan.",
+];
+
+/* If true, the paragraph beneath the header will also
+  rotate. If disabled, the first element of the array
+  will be used for the paragraph.
+  "rotating-paragraphs" div must also be of class
+  "slider"
+*/
+var rotateParagraph = false;
+
+/* Connects paragraph to header slider so they slide
+  at the same time.
+*/
+var connectParagraphToHeaders = false;
+
+/* The paragraphs to rate between. If rotateParagraph disabled,
+  the first element also serves as the only paragraph.
+*/
+var rotatingParagraphs = [
+  "Currently studying at Carleton University for computer science with a specialization in software engineering and a minor in applied linguistics. <span>Whew.</span>"
+];
+```
+
+## Credits
+Made with Font Awesome, Tiny Slider 2, and Skeleton.
---
+++ js/main.js
@@ -0,0 +1,147 @@
+/* IMPORTANT: Each array must have at least 1 element
+  and you must remove all elements within "#rotating-words"
+  and "#rotating-paragraphs".
+  You can also bypass the dynamic elements entirely
+  and uncomment the elements in index.html
+*/
+var loadDynamically = false;
+
+/* The beginning of the header sentence has a black font.
+  The headersBlack element of index i, will be added to
+  the corresponding headersBlue element of index i.
+  E.g. headersBlack[i] + headersBlue[i]
+  headersBlack[0] + headersBlue[0]
+  "Hi, I'm " + "Dan" = "Hi, I'm Dan"
+  
+  If one array is larger than the other, the last element
+  of the smaller array will be used for the rest of the
+  rotations. You can also use empty strings ""
+  E.g. headersBlack[1] doesn't exist
+  "Hi, I'm a student." 
+*/
+var headersBlack = [ //headersBlack + headersBlue, e.g. "Hi, I'm Dan"
+  "Hi, I'm "
+];
+
+var headersBlue = [ //Descriptive and re-affirming words here
+  "Dan.",
+];
+
+/* If true, the paragraph beneath the header will also
+  rotate. If disabled, the first element of the array
+  will be used for the paragraph.
+  "rotating-paragraphs" div must also be of class
+  "slider"
+*/
+var rotateParagraph = false;
+
+/* Connects paragraph to header slider so they slide
+  at the same time.
+*/
+var connectParagraphToHeaders = false;
+
+/* The paragraphs to rate between. If rotateParagraph disabled,
+  the first element also serves as the only paragraph.
+*/
+var rotatingParagraphs = [
+  "Currently studying at Carleton University for computer science with a specialization in software engineering and a minor in applied linguistics. <span>Whew.</span>"
+];
+
+var addRotatingWords = function() {
+  
+  var wordsDiv = document.getElementById("rotating-words");
+  var paragraphsDiv = document.getElementById("rotating-paragraphs");
+  var largestArrayLength;
+  
+  if (!(paragraphsDiv.classList.contains("slider"))) {
+    paragraphsDiv.classList.add("slider");
+  }
+  
+  if (rotateParagraph) {
+    largestArrayLength = Math.max(headersBlack.length, headersBlue.length, rotatingParagraphs.length);
+  } else {
+    largestArrayLength = Math.max(headersBlack.length, headersBlue.length);
+  }
+  
+  for (var index = 0; index < largestArrayLength; index++) {
+    //Temp variables
+    var headerBeginning;
+    var headerEnd;
+    var paragraph = rotatingParagraphs[0]; //Default
+    
+    if (index >= headersBlack.length) { //Use last element
+      headerBeginning = headersBlack[headersBlack.length - 1];
+    } else {
+      headerBeginning = headersBlack[index];
+    }
+    
+    if (index >= headersBlue.length) {
+      headerEnd = headersBlue[headersBlue.length - 1];
+    } else {
+      headerEnd = headersBlue[index];
+    }
+    
+    if (index >= rotatingParagraphs.length) {
+      paragraph = rotatingParagraphs[rotatingParagraphs.length - 1];
+    } else {
+      paragraph = rotatingParagraphs[index];
+    }
+    
+    //Add headers
+    var newDiv = document.createElement("div");
+    newDiv.className = "item";
+    
+    var newElement = document.createElement("h4");
+    var textBeginning = document.createTextNode(headerBeginning);
+    var blueElement = document.createElement("a");
+    var textBlue = document.createTextNode(headerEnd);
+    
+    newElement.appendChild(textBeginning);
+    blueElement.appendChild(textBlue);
+    blueElement.className = "highlighted-words"; //Used for dark theme
+    newElement.appendChild(blueElement);
+    
+    newDiv.appendChild(newElement);
+    wordsDiv.appendChild(newDiv);
+    
+    //Add paragraphs
+    var newPDiv = document.createElement("div");
+    newPDiv.className = "item";
+      
+    var newPElement = document.createElement("p");
+    var textParagraph = document.createTextNode("");
+    newPElement.innerHTML = paragraph; //So you can use html tags inside of paragraph.
+      
+    newPElement.appendChild(textParagraph);
+    if (connectParagraphToHeaders) {
+      newDiv.appendChild(newPElement);  
+    } else {
+      newPDiv.appendChild(newPElement);
+      paragraphsDiv.appendChild(newPDiv);
+    }
+  }
+};
+
+var startTinySlider = function () {
+  var sliders = document.querySelectorAll(".slider");
+  sliders.forEach(slider => {
+    var tnsSlider = tns({
+      container: slider,
+      mode: 'gallery', //carousel is also nice
+      controls: false,
+      nav: false,
+      autoplay: true,
+      autoplayButtonOutput: false,
+      mouseDrag: true
+    });
+  });
+  /*tns({
+    
+  });*/
+}
+
+//On start
+if (loadDynamically) {
+  addRotatingWords();
+}
+startTinySlider();
